b415be3bf7d876f9af6e3215814f4f220509d293
Jonah Wiecek <jonahwiecek@gmail.com>
2022-02-08 15:35:09 +0100

    feat(backend): created initial entities for database with prisma
    Scheme.prisma has been changed to include User, Post, Token and Rating. Seed.ts has been update with simple dummy data
    
    Related to #3
    Development-method: group programming
    Co-authored-by: Kir Truhacev <kirt@stud.ntnu.no>

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,20 +4,52 @@ const prisma = new PrismaClient();
 
 const userData = [
     {
-        name: "test",
-        email: "test@test.com"
+        id: 500,
+        firstName: "Herman",
+        surName: "Seternes",
+        email: "hermahs@stud.ntnu.no",
+        password: "YeetYeet"
+    },
+    {
+        id: 501,
+        firstName: "Johan",
+        surName: "Bjerkem",
+        email: "johabjer@stud.ntnu.no",
+        password: "GreatestPasswordEver"
+    }
+]
+
+const postData = [
+    {
+        timeOfEvent: new Date(2022, 2, 22),
+        city: "Trondheim",
+        venue: "My home",
+        isActive: true,
+        forSale: true,
+        title: "My birthday party :)",
+        description: "My amazing birthday party. Mom will make cake :)",
+        category: "birthday",
+        price: 500.00,
+        authorId: 500
     }
 ]
 
 async function main() {
     console.log("starting seeding");
-    for (const u of userData) {
-        const user = await prisma.user.create({
-            data: u
-        });
+    console.log("----------------");
+    console.log("seeding users")
+    const createManyUsers = await prisma.user.createMany({
+        data: userData
+    });
 
-        console.log(`created user with id: ${user.id}`);
-    }
+    console.log(`Seeding user result: ${createManyUsers}`);
+
+    console.log("seeding posts");
+    const createManyPosts = await prisma.post.createMany({
+        data: postData
+    });
+
+    console.log(`Seeding post result: ${createManyPosts}`);
 
     console.log("seeding finished");
 }
