d8e35f9390870d232d43a0d95f8cc70052f9cfd3
Jonah Niklas Bjørgsvik Wiecek <jnwiecek@stud.ntnu.no>
2022-02-16 13:01:37 +0100

    feat(frontend): wrote a Test component for testing GET request from frontend
    Test.tsx has been added in order to test simple fetching from localhost (GET request). Had to add
    cors to yarn in order for this to work, therefore yarn.lock and package.json are affected. Test can
    be used to get the info of a specified userID by a click of a button.
    
    Related to #21
    Development-method: individual programming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Test from './features/Test';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Switch>
         <Route path="/login">{/* Login page goes here */}</Route>
         <Route path="/posts">{/* Posts page goes here */}</Route>
-        <Route path="/home">{/* Home page goes here */}</Route>
+        <Route path="/home"><Test/></Route>
         <Route path="/">{/* Check if logged in to redirect goes here */}</Route>
       </Switch>
     </Router>
