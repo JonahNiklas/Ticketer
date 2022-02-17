b4310ec21eb3cbb47f69e6cdcc940957862e85fa
saraped <saraped99@gmail.com>
2022-02-17 13:54:23 +0100

    feat(frontend): created login and register page
    Not completely finished but created components.
    
    Related to #9
    
    Co-authored-by: Jonah Niklas Bjørgsvik Wiecek <jnwiecek@ntnu.no>

---
+++ frontend/src/RegisterUser.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+import Logo from "./ticketer_logo.png";
+import { Form, Button } from "react-bootstrap/";
+
+class RegisterUser extends React.Component {
+  render() {
+    return (
+        <div className="registerForm" style={{ padding: "8% 30%" }}>
+          <h3>Registrer ny bruker</h3>
+          <Form>
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>E-mail</Form.Label>
+              <Form.Control type="email" placeholder="E-mail" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formFirstName">
+              <Form.Label>Fornavn</Form.Label>
+              <Form.Control type="name" placeholder="Fornavn" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formLastName">
+              <Form.Label>Etternavn</Form.Label>
+              <Form.Control type="name" placeholder="Etternavn" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formPassword">
+              <Form.Label>Passord</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formRepeatPassword">
+              <Form.Label>Gjenta passord</Form.Label>
+              <Form.Control type="password" placeholder="Repeat password" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Registrer
+            </Button>
+          </Form>
+        </div>
+    );
+  }
+}
+
+export default RegisterUser;
