915f2b2f20472e59c668d66ae54dc447cfaf87d7
Herman Håberg Seternes <hermahs@stud.ntnu.no>
2022-02-22 12:10:55 +0100

    Revert "Merge branch '20-setup-new-branch-for-frontend-design' into 'dev'"
    This reverts merge request !18

--- frontend/src/features/homepage/RecommendedTicketer.tsx
+++ frontend/src/features/homepage/RecommendedTicketer.tsx
@@ -1,91 +1,42 @@
 import React from 'react';
-import { Button, Card, CardGroup, Container, ListGroup, ListGroupItem} from 'react-bootstrap';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCircle} from '@fortawesome/free-solid-svg-icons';
-import '../Menylinje.css';
-
+import { Card, CardGroup, Container } from 'react-bootstrap';
 
 function RecommendedTicketer() {
-  return (
-
-
-      <div className="RecommendedTicketer mt-0 ml-5 mr-5 p-0">
-        <Container>
-          <h2 className='text-center'>Anbefalte Ticketer</h2>
-          <CardGroup>
-
-            <Card style={{ cursor: 'pointer', height: "500px"}} className="mx-4 my-2">
-              <Card.Text className='mb-0'>
-                <button type='button' className='button-2'>
-                  <span className='button__icon-2'>
-                    <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
-                  </span>
-                </button>
+  return(
+    <div className='RecommendedTicketer'>
+      <Container>
+        <h2>Anbefalte Ticketer</h2>
+        <CardGroup>
+          <Card style={{ cursor: 'pointer' }} className='mx-4 my-2'>
+            <Card.Img src='https://picsum.photos/200/200' />
+            <Card.Body>
+              <Card.Title>40 år med vondt brenn&apos;vin</Card.Title>
+              <Card.Text>
+                Vazelina
               </Card.Text>
-              <Card.Img src="https://picsum.photos/200/200" className='mt-0 h-25' />
-              <Card.Body className='m-0'>
-                <Card.Title>Mozarts Requiem</Card.Title>
-                <Card.Text>Gå påsken i møte med et av verdens vakreste verk.
-                Mozarts uendelig vakre Requiem er noe av det nydeligste som er skrevet.
-                </Card.Text>
-                <ListGroup className="list-group-flush">
-                <ListGroupItem>TORSDAG, 7. APRIL, 2022 KL 19.30</ListGroupItem>
-                <ListGroupItem>470,-</ListGroupItem>
-                <Button variant="success">Contact seller</Button>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ cursor: 'pointer', height: "500px"}} className="mx-4 my-2">
-              <Card.Text className='mb-0'>
-                <button type='button' className='button-2'>
-                  <span className='button__icon-3'>
-                    <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
-                    <span style={{ fontSize:"20px",marginLeft:"5px"}}>kir</span>
-                  </span>
-                </button>
+            </Card.Body>
+          </Card>
+          <Card style={{ cursor: 'pointer' }} className='mx-4 my-2'>
+            <Card.Img src='https://picsum.photos/200/200' />
+            <Card.Body>
+              <Card.Title>41 år med vondt brenn&apos;vin</Card.Title>
+              <Card.Text>
+                Vazelina
               </Card.Text>
-              <Card.Img src="https://picsum.photos/200/200" className='mt-0 h-25' />
-              <Card.Body className='m-0'>
-                <Card.Title>Mozarts Requiem</Card.Title>
-                <Card.Text>Gå påsken i møte med et av verdens vakreste verk.
-                Mozarts uendelig vakre Requiem er noe av det nydeligste som er skrevet.
-                </Card.Text>
-                <ListGroup className="list-group-flush">
-                <ListGroupItem>TORSDAG, 7. APRIL, 2022 KL 19.30</ListGroupItem>
-                <ListGroupItem>470,-</ListGroupItem>
-                <Button variant="success">Contact seller</Button>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ cursor: 'pointer', height: "500px"}} className="mx-4 my-2">
-              <Card.Text className='mb-0'>
-                <button type='button' className='button-2'>
-                  <span className='button__icon-3'>
-                    <FontAwesomeIcon icon={faCircle}></FontAwesomeIcon>
-                  </span>
-                </button>
+            </Card.Body>
+          </Card>
+          <Card style={{ cursor: 'pointer' }} className='mx-4 my-2'>
+            <Card.Img src='https://picsum.photos/200/200' />
+            <Card.Body>
+              <Card.Title>42 år med vondt brenn&apos;vin</Card.Title>
+              <Card.Text>
+                Vazelina
               </Card.Text>
-              <Card.Img src="https://picsum.photos/200/200" className='mt-0 h-25' />
-              <Card.Body className='m-0'>
-                <Card.Title>Mozarts Requiem</Card.Title>
-                <Card.Text>Gå påsken i møte med et av verdens vakreste verk.
-                Mozarts uendelig vakre Requiem er noe av det nydeligste som er skrevet.
-                </Card.Text>
-                <ListGroup className="list-group-flush">
-                <ListGroupItem>TORSDAG, 7. APRIL, 2022 KL 19.30</ListGroupItem>
-                <ListGroupItem>470,-</ListGroupItem>
-                <Button variant="success">Contact seller</Button>
-                </ListGroup>
-              </Card.Body>
-            </Card>
-
-          
-            
-          </CardGroup>
-        </Container>
-      </div>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </Container>
+    </div>
   );
 }
 
--- frontend/src/features/homepage/SearchBar.tsx
+++ frontend/src/features/homepage/SearchBar.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SearchBar = () => {
   return (
-    <div className="input-group w-75">
-      <input type="search" className="form-control rounded m-1" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-      <button type="button" className="btn btn-success rounded m-1">search</button>
+    <div className='SearchBar'>
+      <label>Search</label>
+      <input type='text' />
     </div>
   );
-};
+}
 
 export default SearchBar;
