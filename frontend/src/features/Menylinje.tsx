a65ee21cefb3f4579e2fd7bc9a6f8fffba6b0e3a
Jørgen Wiik <jorgerw@stud.ntnu.no>
2022-02-22 12:04:31 +0100

    feat(frontend): connected navbar to existing pages
    The navbar now links to pages that are  created.
    
    Related to #22

--- frontend/src/features/Menylinje.tsx
+++ frontend/src/features/Menylinje.tsx
@@ -4,18 +4,22 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 import {faBell, faPlusCircle, faCircle, faUser,faHouse} from '@fortawesome/free-solid-svg-icons';
 import {faFacebookMessenger} from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 
 function Menylinje() {
     return (
     <nav className='navbar'> 
+        <Link to='/profile' className='link'>
         <button type='button' className='button-1'>
              <span className='button__icon-1'>
                  <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
             </span>
             <span className='button__text-1'>Min ticketer</span>
         </button>
+        </Link>
 
+        <Link to='/home' className='link'>
         <button type='button' className='button-1'>
             <span className='button__icon-2'>
                 <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
@@ -23,6 +27,7 @@ function Menylinje() {
             <span className='button__text-1'>Hjemside</span>
             
         </button>
+        </Link>
         
         <button type='button' className='button-1'>
             <span className='button__icon-2'>
@@ -32,12 +37,14 @@ function Menylinje() {
             
         </button>
 
+        <Link to='/posts' className='link'>
         <button type='button' className='button-1'>
         <span className='button__icon-2'>
             <FontAwesomeIcon icon={faPlusCircle}></FontAwesomeIcon>
         </span>
         <span className='button__text-1'>Ny annonse</span>
         </button>
+        </Link>
 
         <button type='button' className='button-1'>
         <span className='button__icon-2'>
