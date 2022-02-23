9876ed10a8eb79041ef7d1d338167c3db9dd5740
kirtruhacev <kirtruhachev@gmail.com>
2022-02-23 16:35:28 +0100

    feat(frontend): Styling

--- frontend/src/Pages/RegisterUser.tsx
+++ frontend/src/Pages/RegisterUser.tsx
@@ -3,20 +3,22 @@ import Logo from '../images/ticket_logo.png';
 import React from "react";
 import Register from "../features/loginpage/Register";
 import { Card, Container } from "react-bootstrap";
+import Footer from '../features/homepage/Footer';
+import HeaderNoSearchbar from '../features/homepage/HeaderNoSearchbar';
 
 
 const RegisterUser = () => {
 
   return (
     <div>
-      <Container>
+      <HeaderNoSearchbar/>
           <Card className="form">
             <Card.Body>
               <img src={Logo} style={{ margin:'auto', display: 'block'}}/>
               <Register/>
             </Card.Body>
           </Card>
-        </Container>
+          <Footer/>
     </div>
   );
 }
---
+++ frontend/src/features/homepage/HeaderNoSearchbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Col, Row, Image } from 'react-bootstrap';
+import SearchBar from './SearchBar';
+import Logo from '../../images/ticket_logo.png';
+import '../../stylesheets/Posts.css';
+
+
+function Header() {
+  return (
+  <div className='navbar navbar-dark bg-dark'>
+    <Row className='w-100'>
+    <Col className="d-flex justify-content-left">
+          <Image src={Logo}/>
+        </Col>
+
+        </Row>
+
+  </div>
+  )
+}
+
+export default Header;
