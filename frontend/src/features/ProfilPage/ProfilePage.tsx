e174a059ef01740cf86a8cc5597472c500dc313a
kirtruhacev <kirtruhachev@gmail.com>
2022-02-24 11:44:27 +0100

    feat(frontend): Profilepage

--- frontend/src/features/ProfilPage/ProfilePage.tsx
+++ frontend/src/features/ProfilPage/ProfilePage.tsx
@@ -10,6 +10,7 @@ import UserPosts from './UserPosts';
 import '../../stylesheets/ProfileInfo.css';
 import Footer from '../homepage/Footer';
 import Header from '../homepage/Header';
+import { Button, Container, Form } from 'react-bootstrap';
 
 
 
@@ -19,18 +20,30 @@ function ProfilePage() {
       <Menylinje/>
       <div style = {{marginLeft: "133px"}}>       
       <Header/>
-      <div className='row m-3 ms-5'>
-      <div className='col-3'>
+      <Container>
+      <div className='row m-3'>
+      <div className='col'>
         <span className='button__icon-10'><FontAwesomeIcon icon={faCircle}></FontAwesomeIcon></span> 
+        <Form>
+        <Form.Group className="mb-3 w-50" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Endre profilbilde</Form.Label>
+                  <Form.Control type="file" placeholder="Title" />
+                </Form.Group>
+                <Button variant="success mb-3 w-50 ml-50" type="submit" >
+                    Oppdater
+                </Button> 
+        </Form>
       </div>  
       <div className='col'>
         <Profileinfo/>
       </div>   
+      </div>
+      </Container>
       <UserPosts/>      
       </div>
       <Footer/>
       </div>
-    </div>
+    
     );
   }
   
