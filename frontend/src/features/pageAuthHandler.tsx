03956c0b483cabf1ea4bbba50af0a07e895e8a62
Herman Håberg Seternes <hermahs@stud.ntnu.no>
2022-02-26 17:34:36 +0100

    feat(frontend): added protected pages
    Added protected pages such that you do not have access to home, post, and user page.

---
+++ frontend/src/features/pageAuthHandler.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Redirect,
+  Route
+} from 'react-router-dom';
+import {
+  store
+} from '../redux/store';
+
+// is auth => state = store.getState(); if ( state.user.userId && state.token.token ) {valid} else { not valid }
+
+export function ProtectedRoute(props: {children: any, path: string }) {
+
+  const isAuth = store.getState().token.token !== null && store.getState().user.userId !== null;
+
+  console.log(props);
+
+  return ( 
+    <Route {...props.path}> 
+    {isAuth ? props.children : <Redirect to="/login"/>}
+    </Route>
+  )
+}
