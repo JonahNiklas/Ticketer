fc6ade9971a637cd1566a95c3672542c890be649
Johan <johabjer@stud.ntnu.no>
2022-03-01 12:51:36 +0100

    feat(frontend): added successfields on register
    you get a green check if your input is correct
    
    Related to #33
    Edit by: Johan Bjerkem johabjer@stud.ntnu.no

--- frontend/src/features/loginpage/Register.tsx
+++ frontend/src/features/loginpage/Register.tsx
@@ -11,12 +11,58 @@ const RegisterUser = () => {
   const [password, setPassword] = useState<string>("");
   const [repeatPassword, setRepeatPassword] = useState<string>("");
   const [emailError, setEmailError] = useState<boolean>(false);
+  const [emailSuccess, setEmailSuccess] = useState<boolean>(false);
   const [emailErrorMessage, setEmailErrorMessage] = useState<string>("");
   const [firstNameError, setFirstNameError] = useState<boolean>(false);
+  const [firstNameSuccess, setFirstNameSuccess] = useState<boolean>(false);
   const [lastNameError, setLastNameError] = useState<boolean>(false);
+  const [lastNameSuccess, setLastNameSuccess] = useState<boolean>(false);
   const [nameErrorMessage, setNameErrorMessage] = useState<string>("");
   const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [passwordSuccess, setPasswordSuccess] = useState<boolean>(false);
+  const [repeatPasswordSuccess, setRepeatPasswordSuccess] = useState<boolean>(false);
   const [passwordErrorMessage, setPasswordErrorMessage] = useState<string>("");
+
+  const emailCheck = (email: string) => {
+    if(!/^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+      setEmailSuccess(false);
+      return;
+    }
+    setEmailSuccess(true);
+  }
+
+  const firstNameCheck = (name: string) => {
+    if(!/^[A-Z][a-z]{2,15}$/.test(name)){
+      setFirstNameSuccess(false);
+      return;
+    }
+    setFirstNameSuccess(true);
+  }
+
+  const lastNameCheck = (name: string) => {
+    if(!/^[A-Z][a-z]{2,15}$/.test(name)){
+      setLastNameSuccess(false);
+      return;
+    }
+    setLastNameSuccess(true);
+  }
+
+  const passwordCheck = (password: string) => {
+    if(!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password)){
+      setPasswordSuccess(false);
+      return;
+    }
+    setPasswordSuccess(true);
+  }
+
+  const repeatPasswordCheck = (repeatPassword: string) => {
+    console.log(repeatPassword)
+    if(password!==repeatPassword){
+      setRepeatPasswordSuccess(false);
+      return;
+    }
+    setRepeatPasswordSuccess(true);
+  }
 
   async function handleRegister (e: any) {
 
@@ -114,31 +160,54 @@ const RegisterUser = () => {
         <Form.Group className="mb-3" controlId="formRegisterEmail">
           <Form.Label>E-mail</Form.Label>
           {/* function (e) {} */}
-          <Form.Control type="email" placeholder="E-mail" onChange={(e) => {setEmail(e.target.value)}} isInvalid={emailError}/>
+          <Form.Control type="email" placeholder="E-mail" onChange={(e) => 
+            {{setEmail(e.target.value)} emailCheck(e.target.value)}} 
+            isInvalid={emailError} isValid={emailSuccess}/>
           <Form.Control.Feedback type="invalid">{emailErrorMessage}</Form.Control.Feedback>
+          <Form.Control.Feedback type="valid">{}</Form.Control.Feedback>
+
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formFirstName">
           <Form.Label>Fornavn</Form.Label>
-          <Form.Control type="name" placeholder="Fornavn" onChange={(e) => setFirstName(e.target.value)} isInvalid={firstNameError}/>
+          <Form.Control type="name" placeholder="Fornavn" onChange={
+            (e) => 
+            {setFirstName(e.target.value);
+             firstNameCheck(e.target.value);}} isInvalid={firstNameError} isValid={firstNameSuccess}/>
           <Form.Control.Feedback type="invalid">{nameErrorMessage}</Form.Control.Feedback>
+          <Form.Control.Feedback type="valid">{}</Form.Control.Feedback>
+
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formLastName">
           <Form.Label>Etternavn</Form.Label>
-          <Form.Control type="name" placeholder="Etternavn" onChange={(e) => setLastName(e.target.value)} isInvalid={lastNameError}/>
+          <Form.Control type="name" placeholder="Etternavn" onChange={
+            (e) => 
+            {setLastName(e.target.value);
+            lastNameCheck(e.target.value)}} isInvalid={lastNameError} isValid={lastNameSuccess}/>
           <Form.Control.Feedback type="invalid">{nameErrorMessage}</Form.Control.Feedback>
+          <Form.Control.Feedback type="valid">{}</Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formRegisterPassword">
           <Form.Label>Passord</Form.Label>
-          <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} isInvalid={passwordError}/>
+          <Form.Control type="password" placeholder="Password" onChange={
+            (e) => 
+            {setPassword(e.target.value)
+            passwordCheck(e.target.value)}} isInvalid={passwordError} isValid={passwordSuccess}/>
           <Form.Control.Feedback type="invalid">{passwordErrorMessage}</Form.Control.Feedback>
+          <Form.Control.Feedback type="valid">{}</Form.Control.Feedback>
+
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formRepeatPassword">
           <Form.Label>Gjenta passord</Form.Label>
-          <Form.Control type="password" placeholder="Repeat password" onChange={(e) => setRepeatPassword(e.target.value)}/>
+          <Form.Control type="password" placeholder="Repeat password" onChange={
+            (e) =>
+            {setRepeatPassword(e.target.value);
+            repeatPasswordCheck(e.target.value)}} isValid={repeatPasswordSuccess}/>
+          <Form.Control.Feedback type="valid">{}</Form.Control.Feedback>
+
         </Form.Group>
 
         <Button variant="primary" type="submit" onClick={handleRegister}>
