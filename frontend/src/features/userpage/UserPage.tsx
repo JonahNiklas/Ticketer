8307e086ed5130932f7d4177cd5a34b1d09956ea
hermahs <hermahs@stud.ntnu.no>
2022-03-15 16:51:56 +0100

    fix(Frontend): added errorhandling and user not found on user page

--- frontend/src/features/userpage/UserPage.tsx
+++ frontend/src/features/userpage/UserPage.tsx
@@ -18,25 +18,26 @@ import { useLocation } from 'react-router-dom';
 
 function UserPage() {
   const location = useLocation();
-  
 
   const userId = Number.parseInt(location.pathname.split('/')[location.pathname.split('/').length - 1]);
 
   const [firstName, setFirstName] = useState<string>('');
   const [lastName, setLastName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
+  const [userIdError, setError] = useState<boolean>(false);
   
   async function getUserData(){
     try {
-      if (userId !== null) {
+      if (!isNaN(userId)) {
         const user = await getUserById(userId);
         setFirstName(user.firstName);
         setLastName(user.lastName);
         setEmail(user.email);
-
+      } else {
+        setError(true);
       }
     } catch (error: any) {
-      console.error(error);
+      setError(true);
     }
   }
   const [posts, setPosts] = useState<Post[]>([]);
@@ -44,11 +45,11 @@ function UserPage() {
   let rendered = false;
 
   async function getUsersPosts() {
-    if (userId) {
+    if (!isNaN(userId)) {
       try {
         setPosts(await getPostsByAuthorId(userId));
       } catch (error: any) {
-        console.error(error);
+        // console.error(error);
       }
     }
   }
@@ -68,40 +69,48 @@ function UserPage() {
         <Menylinje/>          
         <div style = {{marginLeft: "133px"}}>
           <Header/>
-          <div className='row ms-5'>
-            <div className='col-3 ms-5'>
-              <span className='button__icon-10'><FontAwesomeIcon icon={faUserAstronaut}></FontAwesomeIcon></span>
+          {
+            (userIdError) ? 
+            <div className='row h-100 flex justify-content-center align-items-center text-center' style={{minHeight: "50vh"}}>
+              <h2 >Bruker ble ikke funnet</h2>
             </div>
-            <div className='col-4 mt-5'>
-              <div className="p-3 rounded" style={{ backgroundColor: "rgb(100, 176, 145)" }}>
-                <h5 className="text-white">{firstName} {lastName}</h5>
-                <h5 className="text-white">{email}</h5>
+            :
+            <div>
+              <div className='row ms-5'>
+                <div className='col-3 ms-5'>
+                  <span className='button__icon-10'><FontAwesomeIcon icon={faUserAstronaut}></FontAwesomeIcon></span>
+                </div>
+                <div className='col-4 mt-5'>
+                  <div className="p-3 rounded" style={{ backgroundColor: "rgb(100, 176, 145)" }}>
+                    <h5 className="text-white">{firstName} {lastName}</h5>
+                    <h5 className="text-white">{email}</h5>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-0 ml-5 mr-5 p-0">
+                <Container>
+                  <h2 className="text-center">{firstName} sine Ticketer</h2>
+                  <CardGroup>
+                    {posts.map((post, idx) => (
+                      <PostInfo
+                        key={idx}
+                        createdAt={post.createdAt}
+                        timeOfEvent={post.timeOfEvent}
+                        city={post.city}
+                        venue={post.venue}
+                        forSale={post.forSale}
+                        title={post.title}
+                        description={post.description}
+                        category={post.category}
+                        price={post.price}
+                        authorId={post.authorId}
+                        id={post.id}            />
+                    ))}
+                  </CardGroup>
+                </Container>
               </div>
             </div>
-          </div>
-
-          <div className="mt-0 ml-5 mr-5 p-0">
-          <Container>
-            <h2 className="text-center">{firstName} sine Ticketer</h2>
-            <CardGroup>
-              {posts.map((post, idx) => (
-                <PostInfo
-                  key={idx}
-                  createdAt={post.createdAt}
-                  timeOfEvent={post.timeOfEvent}
-                  city={post.city}
-                  venue={post.venue}
-                  forSale={post.forSale}
-                  title={post.title}
-                  description={post.description}
-                  category={post.category}
-                  price={post.price}
-                  authorId={post.authorId} 
-                  id={post.id}            />
-              ))}
-            </CardGroup>
-          </Container>
-          </div>
+          }
           <Footer/>
         </div>
       </div>
