5aa8c538c0c24aa61eb565e49c9b01229ee41680
Herman Håberg Seternes <hermahs@stud.ntnu.no>
2022-03-28 21:59:48 +0200

    fix(frontend): fixed small issue on registerpage with errorhandling

--- frontend/src/features/homepage/HeaderNoSearchbar.tsx
+++ frontend/src/features/homepage/HeaderNoSearchbar.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Col, Row, Image } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import Logo from '../../images/ticket_logo.png';
 import '../../stylesheets/Posts.css';
 
 function Header() {
+  const history = useHistory();
   return (
     <div className="navbar navbar-dark bg-dark">
       <Row className="w-100">
         <Col className="d-flex justify-content-left">
-          <Image src={Logo} className="ms-5" />
+          <Image src={Logo} className="ms-5" onClick={() => history.push('/')} role='button'/>
         </Col>
       </Row>
     </div>
